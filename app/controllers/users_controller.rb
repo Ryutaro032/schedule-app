616987f1295dadaacb34edc614153fe96e12c43e
Ryutaro032 <72485351+Ryutaro032@users.noreply.github.com>
2023-08-03 19:26:12 +0900

    編集画面と更新の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(params.require(:user).permit(:title, :start_day, :end_day, :all_day, :memo))
+      flash[:notice] = "「#{@user.title}」が更新されました"
+      redirect_to :users
+    else
+      render "edit"
+    end
   end
 
   def destroy
